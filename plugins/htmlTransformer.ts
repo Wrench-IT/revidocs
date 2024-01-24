7476023bba1b12e41e3e9625355514ec514360f0
Denis Borovensky <denisborovensky@gmail.com>
2024-01-24 20:11:36 +0300

    feat: add 404 redirect page for advanced routing

--- plugins/htmlTransformer.ts
+++ plugins/htmlTransformer.ts
@@ -1,4 +1,4 @@
-import { indexHtml } from '../settings';
+import { indexHtml, baseUrl } from '../settings';
 import metaTags from './metaTags';
 
 const htmlTransformer = () => {
@@ -6,7 +6,11 @@ const htmlTransformer = () => {
     name: 'html-transformer',
     transformIndexHtml(html: string): string {
       return html
-        .replace(/(<html lang="en">)/, `<html lang="${indexHtml.lang}">`)
+        .replace(/(<html lang="(.*?)">)/, `<html lang="${indexHtml.lang}">`)
+        .replace(
+          /(<link rel="icon" type="image\/svg\+xml" href="(.*?)" \/>)/,
+          `<link rel="icon" type="image/svg+xml" href="${baseUrl}favicon.svg" />`,
+        )
         .replace(/(<title>(.*?)<\/title>)/, `<title>${indexHtml.title}</title>`)
         .replace(/(<title>(.*?)<\/title>)/, `$1${metaTags}`);
     },
--- plugins/metaTags.ts
+++ plugins/metaTags.ts
@@ -8,6 +8,20 @@ const metaTags: string = `
     <meta property="og:image" content="${indexHtml.ogImage}">
     <meta property="og:url" content="${indexHtml.ogUrl}">
     <meta name="twitter:title" content="${indexHtml.twitterTitle}">
-    <link rel="canonical" href="${indexHtml.canonicalUrl}"/>`;
+    <link rel="canonical" href="${indexHtml.canonicalUrl}"/>
+    <!-- Start redirect to SPA -->
+    <script type="text/javascript">
+      (function(l) {
+        if (l.search[1] === '/' ) {
+          var decoded = l.search.slice(1).split('&').map(function(s) { 
+            return s.replace(/~and~/g, '&')
+          }).join('?');
+          window.history.replaceState(null, null,
+              l.pathname.slice(0, -1) + decoded + l.hash
+          );
+        }
+      }(window.location))
+    </script>
+    <!-- End redirect to SPA -->`;
 
 export default metaTags;
