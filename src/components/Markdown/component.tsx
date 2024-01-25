0ccc36dbeb7db13ecdf9112844a89fed90540c51
Denis Borovensky <denisborovensky@gmail.com>
2024-01-26 00:16:41 +0300

    fix: change basic typography typing

--- src/components/Markdown/component.tsx
+++ src/components/Markdown/component.tsx
@@ -11,19 +11,19 @@ const MarkdownComponent: React.FC<{
 }> = ({ children }) => (
   <Markdown
     components={{
-      h1: Title,
-      h2: (props) => <Title level={2} {...props} />,
-      h3: (props) => <Title level={3} {...props} />,
-      h4: (props) => <Title level={4} {...props} />,
-      h5: (props) => <Title level={5} {...props} />,
+      h1: (props) => <Title level={1}>{props.children}</Title>,
+      h2: (props) => <Title level={2}>{props.children}</Title>,
+      h3: (props) => <Title level={3}>{props.children}</Title>,
+      h4: (props) => <Title level={4}>{props.children}</Title>,
+      h5: (props) => <Title level={5}>{props.children}</Title>,
       p: (props) => (
         <p>
-          <Text {...props} />
+          <Text>{props.children}</Text>
         </p>
       ),
-      em: (props) => <Text italic {...props} />,
-      strong: (props) => <Text strong {...props} />,
-      del: (props) => <Text delete {...props} />,
+      em: (props) => <Text italic>{props.children}</Text>,
+      strong: (props) => <Text strong>{props.children}</Text>,
+      del: (props) => <Text delete>{props.children}</Text>,
     }}
     remarkPlugins={[remarkGfm]}
   >
